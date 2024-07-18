12417e7f8b5b2e3c9e7f6e2f9c2a53b44edc12a0
kapo1123 <kpk23@byu.edu>
2024-07-18 17:25:38 -0600

    Added model's objects in admin

--- backend/playlists/admin.py
+++ backend/playlists/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+# backend/playlists/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Playlist, Song, Vote
+
+# Register your models here
+admin.site.register(Playlist)
+admin.site.register(Song)
+admin.site.register(Vote)

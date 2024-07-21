5a5f1b35d043c8af15104a6803bddc2b5d3645ae
kapo1123 <kpk23@byu.edu>
2024-07-20 19:50:57 -0600

    Added an exampleconfig file

---
+++ backend/config_example.py
@@ -0,0 +1,17 @@
+# backend/config.py
+
+# Spotify API credentials
+SPOTIFY_CLIENT_ID = 'placeholder'
+SPOTIFY_CLIENT_SECRET = 'placeholder'
+SPOTIFY_REDIRECT_URI = 'placeholder'
+
+# OpenAI API key
+OPENAI_API_KEY = 'sk-placeholder'
+OPENAI_ORGID =  "placeholder"
+
+DB_CONFIG = {
+    'host': 'placeholder',
+    'user': 'placeholder',
+    'password': 'placeholder',
+    'database': 'placeholder'
+}
